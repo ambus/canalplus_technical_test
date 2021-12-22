8fbaa955c9353f393aae896be8464a66fce850d2
Szymon Standarski <szymon.standarski@billennium.com>
2021-12-22 15:24:16 +0100

    feat(FilmItem): display images

--- src/components/FilmItem/FilmItem.test.tsx
+++ src/components/FilmItem/FilmItem.test.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable testing-library/no-container */
 /* eslint-disable testing-library/no-node-access */
 import { render, screen } from "@testing-library/react";
-import FilmItem, { FilmItemProps } from "./FilmItem";
+import FilmItem, { FilmItemProps, IMG_BASE_URL } from "./FilmItem";
 
-describe("trimString", () => {
+describe("filmItem", () => {
   let mockFilmData: FilmItemProps;
 
   beforeEach(() => {
@@ -13,6 +13,7 @@ describe("trimString", () => {
         ["cast", "John Kowalski"],
         ["release date", "2013-12-11"],
       ],
+      img: "/extraImage.jpg",
     };
   });
 
@@ -20,6 +21,27 @@ describe("trimString", () => {
     render(<FilmItem name={mockFilmData.name} />);
     const nameElement = screen.getByText(/Test film name/i);
     expect(nameElement).toBeInTheDocument();
+  });
+
+  it("should render the FilmItem with the correct url, consisting of a fixed part and a given file name", () => {
+    render(<FilmItem name={mockFilmData.name} img={mockFilmData.img} />);
+    const imgElement = screen.getByRole("img");
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement.getAttribute("src")).toBe(
+      IMG_BASE_URL + mockFilmData.img
+    );
+  });
+
+  it("should render the FilmItem without image if a given img parameter is empty", () => {
+    render(<FilmItem name={mockFilmData.name} img={""} />);
+    const imgElement = screen.queryByRole("img");
+    expect(imgElement).toBeNull();
+  });
+
+  it("should render the FilmItem without image if a given img parameter is undefinded", () => {
+    render(<FilmItem name={mockFilmData.name} />);
+    const imgElement = screen.queryByRole("img");
+    expect(imgElement).toBeNull();
   });
 
   it("should render the FilmItem without any description element if extraDescriptions param is empty", () => {
@@ -45,4 +67,24 @@ describe("trimString", () => {
       mockFilmData?.extraDescriptions?.length
     );
   });
+
+  // it("should render the FilmItem with the same number of extraDescriptions fileds", () => {
+  //   const { container } = render(
+  //     <FilmItem
+  //       name={mockFilmData.name}
+  //       extraDescriptions={mockFilmData.extraDescriptions}
+  //     />
+  //   );
+
+  //   const extraDescriptionWrapper = container.querySelectorAll(
+  //     ".film-card__extra-description"
+  //   );
+
+  //   extraDescriptionWrapper.forEach((element) => {
+  //     console.warn(element)
+  //     expect(
+  //       (element.childNodes[0] as HTMLDivElement).innerText.toLocaleLowerCase()
+  //     ).toContain(mockFilmData?.extraDescriptions?.[0][0].toLocaleLowerCase());
+  //   });
+  // });
 });
--- src/components/FilmItem/FilmItem.tsx
+++ src/components/FilmItem/FilmItem.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from "react";
 import "./FilmItem.css";
 
+export const IMG_BASE_URL = "http://image.tmdb.org/t/p/w300";
+
 export interface FilmItemProps {
   name: string;
   extraDescriptions?: [name: string, value: string][];
+  img?: string;
 }
 
-export const FilmItem: FC<FilmItemProps> = ({ name, extraDescriptions }) => {
+export const FilmItem: FC<FilmItemProps> = ({
+  name,
+  extraDescriptions,
+  img,
+}) => {
   const getExtraDescription = () => {
     return extraDescriptions?.map((description) => (
       <div key={description[0]} className="film-card__extra-description">
@@ -16,8 +23,16 @@ export const FilmItem: FC<FilmItemProps> = ({ name, extraDescriptions }) => {
     ));
   };
 
+  const getImage = () => {
+    if (!img || img.length <= 0) {
+      return null;
+    }
+    return <img src={IMG_BASE_URL + img} alt="Backtrop"></img>;
+  };
+
   return (
     <div className="film-card">
+      {getImage()}
       <h2>{name}</h2>
       <div className="film-card__body">{getExtraDescription()}</div>
     </div>
