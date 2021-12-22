d624b83708aa5b375de056ae7c78e11b91a07558
Szymon Standarski <szymon.standarski@billennium.com>
2021-12-22 15:24:40 +0100

    feat(App): connect all components on the App page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,67 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { TVShow } from "./models";
+import * as useTVShowsApi from "./services/tvshows.service";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  const mockTVShows: TVShow[] = [
+    {
+      popularity: 0,
+      id: 1,
+      media_type: "tv",
+      original_title: "Test tvShow",
+      overview:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at est dictum, viverra magna nec, luctus nibh. Integer ut volutpat metus. Nulla tincidunt pretium quam eget laoreet. Donec feugiat in nisi.",
+    },
+  ];
+
+  const mockState: useTVShowsApi.State<TVShow[]> = {
+    isLoading: false,
+    isError: false,
+    data: mockTVShows,
+  };
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+    jest.spyOn(global, "setTimeout");
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should display header with image and label", () => {
+    render(<App />);
+    const headerElement = screen.getByText(/TECHNICAL TEST/i);
+    const imgElement = screen.getByRole("img");
+
+    expect(headerElement).toBeInTheDocument();
+    expect(imgElement).toBeInTheDocument();
+  });
+
+  it("should display search input", async () => {
+    render(<App />);
+    const inputElement = await screen.findByRole("textbox");
+
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should display text 'Nothing to show' if tvshow list is empty", () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Nothing to show/i);
+
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it("should display tvshows list if tvShows are not empty", () => {
+    jest
+      .spyOn(useTVShowsApi, "useTVShowsApi")
+      .mockReturnValue([mockState, jest.fn()]);
+
+    render(<App />);
+    const firstTvShow = screen.getByText(
+      mockTVShows[0].original_title ?? "INVALID_TEXT"
+    );
+    expect(firstTvShow).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,53 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { SearchInput } from "./components/SearchInput";
+import { useTVShowsApi } from "./services";
+import { FilmItem } from "./components/FilmItem";
+import { trimString } from "./utils";
+
+export const OVERVIEW_MAX_LENGTH = 150;
 
 export const App = () => {
+  const [tvShows, fetchTvShows] = useTVShowsApi([]);
+
+  const getTvShows = () => {
+    if (!tvShows || !tvShows.data || tvShows.data.length <= 0) {
+      return <span>Nothing to show</span>;
+    }
+    return tvShows?.data?.map((tvshow) => {
+      const extraDescriptions: [name: string, value: string][] = [];
+
+      extraDescriptions.push([
+        "Release date",
+        `${tvshow.release_date ?? tvshow.first_air_date}`,
+      ]);
+
+      if (tvshow.overview && tvshow.overview?.length > 0) {
+        extraDescriptions.push([
+          "Summary",
+          `${trimString(tvshow.overview, OVERVIEW_MAX_LENGTH)}`,
+        ]);
+      }
+
+      if (tvshow.origin_country && tvshow.origin_country?.length > 0) {
+        extraDescriptions.push(["Country", `${tvshow.origin_country}`]);
+      }
+
+      if (tvshow.cast && tvshow.cast?.length > 0) {
+        extraDescriptions.push(["Cast", `${tvshow.cast}`]);
+      }
+
+      return (
+        <FilmItem
+          key={tvshow.id}
+          name={tvshow.original_title ?? tvshow.name ?? "undefined"}
+          extraDescriptions={extraDescriptions}
+          img={tvshow.backdrop_path}
+        ></FilmItem>
+      );
+    });
+  };
+
   return (
     <div className="app">
       <header className="app__header">
@@ -19,10 +64,16 @@ export const App = () => {
             id="search"
             name="search"
             onChange={(value) => {
-              console.log(value);
+              if (value && value?.length > 0) {
+                fetchTvShows(value);
+              } else {
+                fetchTvShows("");
+              }
             }}
           ></SearchInput>
         </div>
+
+        <div className="body__film-list">{getTvShows()}</div>
       </div>
     </div>
   );
